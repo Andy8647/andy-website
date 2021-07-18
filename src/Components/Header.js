e1a0cc1c998715b70773a4675a37572ad3d051d9
Andy8647 <andy8647lgc@gmail.com>
2021-07-17 21:36:52 -0400

    basic done

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -1,6 +1,8 @@
 import React, { Component } from "react";
 import ParticlesBg from "particles-bg";
 import Fade from "react-reveal";
+import Typist from "react-typist";
+import "react-typist/dist/Typist.css";
 
 class Header extends Component {
   render() {
@@ -59,7 +61,15 @@ class Header extends Component {
         <div className="row banner">
           <div className="banner-text">
             <Fade bottom>
-              <h1 className="responsive-headline">{name}</h1>
+              <h1 className="responsive-headline">
+                <Typist
+                  avgTypingDelay={100}
+                  startDelay={1500}
+                  cursor={{ blink: true }}
+                >
+                  {name}
+                </Typist>
+              </h1>
             </Fade>
             <Fade bottom duration={1200}>
               <h3>{description}.</h3>
